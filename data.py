78344880c3901c74d017655121c5ea37843e979b
Jeremy Keeton <jkeeton@tableau.com>
2018-01-27 14:57:13 -0800

    NewFlaskApp

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Here are my Python Selenium projects',
+            'author': 'Jeremy Keeton',
+            'create_date': '10-13-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Im learning visualization with Python on this page here',
+            'author': 'Jeremy Keeton',
+            'create_date': '10-13-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor.',
+            'author': 'Jeremy Keeton',
+            'create_date': '10-13-2017'
+        }
+    ]
+    return articles
